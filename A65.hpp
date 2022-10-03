57e65af1cbe22d8526672a91c7affdf08924d7b9
Ayaan Ahmad <ayanahmad175@gmail.com>
2022-10-02 17:07:24 -0700

    failed test

--- A65.hpp
+++ A65.hpp
@@ -3,10 +3,6 @@
 #include <iomanip>
 #include <iostream>
 using namespace std;
-void getinput(int n1, int n2,int n3);
-void swap (int num1, int num2, int num3);
-
-
 
 void getinput(int n1, int n2,int n3) {
     cout << "enter number 1: ";
@@ -16,12 +12,16 @@ void getinput(int n1, int n2,int n3) {
     cout << "enter number 3: ";
     cin >> n3;
 }
-void swap (int num1, int num2, int num3){
-    cout << num3 << " " << num2 << " " << num1 << " ";
+void swap (int n1, int n2, int n3){
+    cout << num3 << " " << num1 << " "  << num2 << " ";
+}
+
+void swaptwo (int n1, int n2){
+    cout << num2 << " " << num1;
 }
 // complete two functions
 // swap() ; to swap all three values like (a, b, c) to (c, a, b)
 //              This function will use swaptwo().
 
-// swaptwo() ; to swap two values (a, b) to (b, a)
+// swaptwo() ; to swap two value (a, b) to (b, a)
  
