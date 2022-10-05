d8ed952133a6aad7251331eb797ee6c1bf2aa3c5
Ayaan Ahmad <ayanahmad175@gmail.com>
2022-10-04 18:10:13 -0700

    it didnt work according to the test.

--- A65.hpp
+++ A65.hpp
@@ -13,11 +13,23 @@ void getinput(int n1, int n2,int n3) {
     cin >> n3;
 }
 void swap (int n1, int n2, int n3){
-    cout << num3 << " " << num1 << " "  << num2 << " ";
+    int tn1,tn2,tn3;
+    tn1 = n1;
+    tn2 = n2;
+    tn3 = n3;
+    n1 = tn3;
+    n2 = tn1;
+    n3 = tn2;
+    cout << n1 << n2 << n3; 
 }
 
 void swaptwo (int n1, int n2){
-    cout << num2 << " " << num1;
+     int tn1,tn2;
+    tn1 = n1;
+    tn2 = n2;
+    n1 = tn2;
+    n2 = tn1; 
+    cout << n1 << n2; 
 }
 // complete two functions
 // swap() ; to swap all three values like (a, b, c) to (c, a, b)
